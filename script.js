9ae259e9f88b25ab57019c342d0981d6de3f4135
josytech <josytech4@gmail.com>
2022-01-24 21:05:46 -0300

    desafio 01

---
+++ script.js
@@ -0,0 +1,28 @@
+// Crie uma variável que receba um nome;
+
+let nome = "Joseane"
+console.log(nome)
+// Crie uma variável que receba um número;
+
+let number = 10
+console.log(number)
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let anoAtual = 2022 - 10
+console.log(anoAtual)
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+anoAtual = 2022 + 10
+console.log(anoAtual)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log("Estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quartaFeira = true
+console.log(quartaFeira)
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+console.log(typeof quartaFeira)
